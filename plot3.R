93da227e12ae99a2adeb7ed8e92e017e488a5082
Natalia <natvau@gmail.com>
2014-08-11 09:32:31 +1000

    third plot

---
+++ plot3.R
@@ -0,0 +1,20 @@
+## load library
+install.packages('sqldf')
+library(sqldf)
+
+## make a path and file string
+fn<-"household_power_consumption.txt"
+
+## read file and select rows based on string "1/1/2007" and store in dataframe DF
+data <- read.csv.sql(fn, sep=";",sql = 'select * from file where Date = "1/2/2007" or Date = "2/2/2007"')
+
+## doing the graph and exporting it to a png file
+png(filename = "plot3.png", width = 480, height = 480)
+
+time <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+plot(time, data$Sub_metering_1, ylab = "Energy sub metering", xlab='', type='n')
+lines(time, data$Sub_metering_1, col='black')
+lines(time, data$Sub_metering_2, col='red')
+lines(time, data$Sub_metering_3, col='blue')
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c('black','red','blue'))
+dev.off()
